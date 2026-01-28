19f101759f6df66a008a99543c40bf9cdfc984a9
muhammadanascode <anassohal34343@gmail.com>
2026-01-28 18:11:41 +0500

    feat: created a method to create new show

--- frontend/src/services/show.js
+++ frontend/src/services/show.js
@@ -1,5 +1,6 @@
 import { supabase } from "../config/supabase.js";;
 
+//Service: Fetch all shows for a user
 export const getShows = async (userId) => {
   if (!userId) return [];
   const { data, error } = await supabase
@@ -13,4 +14,32 @@ export const getShows = async (userId) => {
     return;
   }
   return data;
+};
+
+// Service: Create a new show
+export const createShow = async (showTitle, showDescription, userId) => {
+  const { data, error } = await supabase
+    .from("shows")
+    .insert([
+      {
+        title: showTitle,
+        description: showDescription ?? null,
+        owner_id: userId,
+      },
+    ])
+    .select()
+    .single();
+
+  if (error) {
+    console.error("Failed to create show:", error);
+    return {
+      success: false,
+      error: error.message || "Failed to create show",
+    };
+  }
+
+  return {
+    success: true,
+    showId: data.id,
+  };
 };
